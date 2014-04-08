f03e394310063e673a50f8a19f89e8ffc5e821c7
Orlin M Bozhinov <om@soundsapiens.com>
2014-04-08 14:54:55 +0300

    a list of commands that produce commands to eval

--- bin/daps.sh
+++ bin/daps.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-if [ $1 = "ssh" ]; then
-  # daps.js stdout command
+# determine if a list $1 contains an item $2
+contains() {
+  for word in $1; do
+    [[ $word = $2 ]] && return 0 # = true
+  done
+  return 1 # = false
+}
+
+# Space-separated list of commands that produce commands to eval.
+# Be careful what goes here - running arbitrary strings can be bad!
+# TODO: make sure it's a one-line, at least, also strip the /\n$/?
+evalist="ssh empty"
+
+if contains $evalist $1 ; then
+  # eval daps.js stdout command
   eval $(daps.js $*)
 else
   daps.js $*
