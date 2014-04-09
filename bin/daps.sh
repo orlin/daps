cc43a59b58699e2d560e96a56004020d31b9001b
Orlin M Bozhinov <om@soundsapiens.com>
2014-04-09 18:05:36 +0300

    make sure we are in the right place, or don't run anything

--- bin/daps.sh
+++ bin/daps.sh
@@ -23,16 +23,19 @@ oneline() {
 
 # Is running in a sub-shell necessary in order for the `cd path` to be temporary?
 ( path=$(daps.js path)
-  oneline path
-  cd $path
-  # TODO: if $path is '.' then make sure we are in the right place, or warn.
-
-  if contains "$evalist" $1 ; then
-    # eval daps.js <command>
-    command=$(daps.js $*)
-    oneline $* "$command"
-    eval $command
+  oneline path && cd $path
+  # Make sure we are in the right place, or don't run anything.
+  if grep -q "^# daps --" "$path/README.md"; then
+    if contains "$evalist" $1 ; then
+      # eval daps.js <command>
+      command=$(daps.js $*)
+      oneline $* "$command"
+      eval $command
+    else
+      daps.js $*
+    fi
   else
-    daps.js $*
+    echo "This '$path' path is not the root directory of daps."
+    echo "Best set the \$NODE_PATH - or else cd to where daps is found."
   fi
 )
